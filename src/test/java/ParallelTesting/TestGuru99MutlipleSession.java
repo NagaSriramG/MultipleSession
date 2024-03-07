e666d9eadc193c5fb15b1019a191b518e675a989
NagaSriramG <sriramgarikipati@gmail.com>
2024-03-07 20:28:17 +0530

    Firstcommit

---
+++ src/test/java/ParallelTesting/TestGuru99MutlipleSession.java
@@ -0,0 +1,45 @@
+package ParallelTesting;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class TestGuru99MutlipleSession {
+	WebDriver driver;
+    @Test    
+    public void executeSessionOne(){
+            //First session of WebDriver
+            driver = new EdgeDriver();
+            //Goto guru99 site
+            driver.get("http://demo.guru99.com/V4/");
+            //find user name text box and fill it
+            driver.findElement(By.name("uid")).sendKeys("Driver 1");
+            
+        }
+        
+    @Test    
+        public void executeSessionTwo(){
+            //Second session of WebDriver
+        driver = new EdgeDriver();
+            //Goto guru99 site
+        driver.get("http://demo.guru99.com/V4/");
+        //find user name text box and fill it
+        driver.findElement(By.name("uid")).sendKeys("Driver 2");
+        
+        }
+        
+    @Test    
+        public void executeSessionThree(){
+            //Third session of WebDriver
+        driver = new EdgeDriver();
+            //Goto guru99 site
+        driver.get("http://demo.guru99.com/V4/");
+        //find user name text box and fill it
+        driver.findElement(By.name("uid")).sendKeys("Driver 3");
+        
+        }        
+}
+
+	
+
